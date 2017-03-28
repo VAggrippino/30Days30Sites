a03a3f17605d72d49ad90bc8d9fefe0aba52594a
Vince Aggrippino <yhqktpi@gmail.com>
2017-03-28 13:29:59 +0800

    Add images with lightbox and adjust CSS for larger screens

--- 07_joker_rising/js/main.js
+++ 07_joker_rising/js/main.js
@@ -1,5 +1,5 @@
+// Load a different slogan each time the page loads.
 const slogan = document.getElementById('slogan');
-
 const slogans = [
   'Get the Joke.',
   'Get the Joke?',
@@ -10,6 +10,7 @@ const slogans = [
 const sloganNumber = Math.floor(Math.random() * slogans.length);
 slogan.innerText = slogans[sloganNumber];
 
+// Add click handling for the video.
 const trailer = document.getElementById('trailer');
 trailer.addEventListener('click', e => {
   const video = trailer.getElementsByTagName('video')[0];
@@ -18,6 +19,14 @@ trailer.addEventListener('click', e => {
   video[action]();
 });
 
+// Remove the play / pause class from the trailer so that the indicator properly
+// disappears.
 trailer.addEventListener('animationend', e => {
   trailer.classList.remove('pause', 'play');
+});
+
+// Lightbox Options
+// from http://lokeshdhakar.com/projects/lightbox2/
+lightbox.option({
+  "disableScrolling": true
 });
