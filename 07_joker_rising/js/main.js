d9dc6ab6474713536fc32a7a001d1fb9615dbcd5
Vince Aggrippino <yhqktpi@gmail.com>
2017-03-28 08:57:29 +0800

    Better click handling for video

--- 07_joker_rising/js/main.js
+++ 07_joker_rising/js/main.js
@@ -10,8 +10,14 @@ const slogans = [
 const sloganNumber = Math.floor(Math.random() * slogans.length);
 slogan.innerText = slogans[sloganNumber];
 
-const video = document.getElementById('video');
-video.addEventListener('click', e => {
+const trailer = document.getElementById('trailer');
+trailer.addEventListener('click', e => {
+  const video = trailer.getElementsByTagName('video')[0];
   const action = video.paused ? 'play' : 'pause';
+  trailer.classList.add(action);
   video[action]();
+});
+
+trailer.addEventListener('animationend', e => {
+  trailer.classList.remove('pause', 'play');
 });
