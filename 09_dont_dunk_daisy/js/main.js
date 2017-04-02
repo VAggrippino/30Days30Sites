eb780648b0ec4cba4a6d8a3198910ad1a7629a20
Vince Aggrippino <yhqktpi@gmail.com>
2017-04-03 00:40:44 +0800

    Completed functionality

--- 09_dont_dunk_daisy/js/main.js
+++ 09_dont_dunk_daisy/js/main.js
@@ -6,6 +6,7 @@ const words = [
 ];
 const n = Math.floor(Math.random() * words.length);
 
+// Create the blanks
 const solution = document.getElementById('solution');
 const blanks = solution.querySelector('.blanks');
 const clue = solution.querySelector('.clue');
@@ -14,8 +15,38 @@ for (let i = 0; i < words[n].word.length; i++) {
   const letter = words[n].word[i];
   const div = document.createElement('div');
   div.classList.add('blank');
+  div.dataset.letter = letter;
   div.innerText = letter;
   blanks.appendChild(div)
 }
 
 clue.style.backgroundImage = `url(${words[n].image})`;
+
+// Set up what happens when clicking the letters
+const platform = document.getElementById('platform');
+let errors = 0;
+let gameOver = false;
+const letters = document.querySelectorAll('.letters .letter');
+letters.forEach(letter => {
+  letter.addEventListener('click', e => {
+    if (letter.classList.contains('used')) return false;
+    if (gameOver) return false;
+    const value = letter.innerText.toLowerCase();
+    letter.classList.add('used');
+
+    if (words[n].word.indexOf(value) !== -1) {
+      blanks.querySelectorAll('.blank').forEach(blank => {
+        if (blank.dataset.letter == value) blank.classList.add('show');
+      });
+    } else {
+      errors++;
+      platform.classList.remove('open1', 'open2', 'open3', 'open4', 'open5');
+      platform.classList.add(`open${errors}`);
+
+      if (errors == 5) {
+        platform.classList.add('open');
+        gameOver = true;
+      }
+    }
+  });
+});
