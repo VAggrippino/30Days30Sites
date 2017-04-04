ed4c82104bcdc6a2fa75ccdfe64594f697322274
Vince Aggrippino <yhqktpi@gmail.com>
2017-04-04 08:05:13 +0800

    Add lightbox and tweak CSS for different size screens

--- 10_photographer/js/main.js
+++ 10_photographer/js/main.js
@@ -1,4 +1,5 @@
-document.querySelectorAll('[data-image]').forEach(block => {
+const lightbox = document.getElementById('lightbox');
+document.querySelectorAll('.photo').forEach(block => {
   block.style.backgroundImage = `url(${block.dataset.image})`;
 });
 
@@ -12,4 +13,19 @@ $('.carousel').slick({
   variableWidth: true,
   autoplay: true,
   autoplaySpeed: 2000
+});
+
+// Show the lightbox display when clicking on an image in the carousel.
+document.querySelector('.carousel').addEventListener('click', e => {
+  if (e.target.tagName == 'A' && e.target.classList.contains('photo')) {
+    e.preventDefault();
+    const image = e.target.dataset.image;
+    document.body.classList.add('showLightbox');
+    lightbox.getElementsByTagName('img')[0].setAttribute('src', image);
+  }
+});
+
+// Make the lightbox go away when clicked.
+lightbox.addEventListener('click', e => {
+  document.body.classList.remove('showLightbox');
 });
